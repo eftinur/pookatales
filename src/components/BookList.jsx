b4b1ab5c25439e8422864dc90de49ac1a956bab0
eftinur <eftinur90@gmail.com>
2023-06-08 13:29:26 +0600

    fixed some API issues

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const BookList = () => {
   const [data, setData] = useState([]);
   // GEtting data from API
-  const API_URL = "https://example-data.draftbit.com/books?_limit=25"; // API URL
+  const API_URL = "https://example-data.draftbit.com/books?_limit=28"; // API URL
 
   useEffect(() => {
     axios.get(API_URL).then((res) => {
@@ -11,15 +11,15 @@ const BookList = () => {
       console.log(data);
     });
   }, []);
+
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 my-20">
       <div className="grid grid-cols-4 gap-20">
         {data.map((book, index) => (
-          <div className="book_wrapper" key={index}>
+          <div className="book_wrapper shadow-xl" key={index}>
             <div className="book_poster">
               <img src={book?.image_url} alt="" />
             </div>
-           
           </div>
         ))}
       </div>
